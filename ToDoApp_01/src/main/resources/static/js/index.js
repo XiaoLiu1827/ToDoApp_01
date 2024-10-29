ba12363e250289d7525748ed6104348671a39b5e
ryuss <ryuss@192.168.0.4>
2024-10-29 22:37:32 +0900

    貯金時の挙動

--- ToDoApp_01/src/main/resources/static/js/index.js
+++ ToDoApp_01/src/main/resources/static/js/index.js
@@ -4,7 +4,19 @@ selectElement.addEventListener(`change`, function() {
 	let defaultSavingsAmount = selectedOption.getAttribute(`data-default-savings-amount`);
 	console.log('Selected Rule ID:', selectElement.value);
 	console.log('Default Savings Amount:', defaultSavingsAmount);
-	
+
 	const inputAmount = document.getElementById(`amount`);
 	inputAmount.value = defaultSavingsAmount;
 });
+
+function setMyRuleData(button) {
+	const myRuleId = button.getAttribute("data-id");
+	const myRuleName = button.getAttribute("data-name");
+
+	// 隠しフィールドに設定
+	document.getElementById("myRuleId").value = myRuleId;
+	document.getElementById("myRuleName").value = myRuleName;
+
+	// フォームを送信
+	button.closest("form").submit();
+}
