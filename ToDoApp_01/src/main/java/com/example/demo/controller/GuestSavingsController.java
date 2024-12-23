2907deb1a255fb53225088f1e11fc7c56e8044bc
ryuss <ryuss@192.168.0.7>
2024-12-23 20:38:54 +0900

    index用パスを修正

--- ToDoApp_01/src/main/java/com/example/demo/controller/GuestSavingsController.java
+++ ToDoApp_01/src/main/java/com/example/demo/controller/GuestSavingsController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/savings")
+@RequestMapping("/")
 public class GuestSavingsController {
 
 	@GetMapping
